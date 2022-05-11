518a1f2c8fae567e10fc0de63ce3392d16dfc796
Garza <mgarza@friendsbalt.org>
2022-05-11 12:17:28 -0400

    curl stuff present

--- api.php
+++ api.php
@@ -1,11 +1,22 @@
 <?php
 	session_start();
-	
+	// create curl resource
+	$ch = curl_init();
+
+	// set url
+	curl_setopt($ch, CURLOPT_URL, "api.kanye.rest");
+
+	//return the transfer as a string
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+	// $output contains the output string
+	$output = curl_exec($ch);
+
+	echo $output;
+	// close curl resource to free up system resources
+	curl_close($ch);     
 	
 
-		http_response_code(400);
-		echo("RUH ROH NO GUESSES LEFT LOSER YOU SUCK!");
-		exit();
 	
 
 	

2efe3b6b1967a7a6325f9970b85546287a4687b3
Garza <mgarza@friendsbalt.org>
2022-05-19 09:44:22 -0400

    make the button and connect HTML to JS

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function loadQuote(){
+    getRequest();
+}
+
+
+function getRequest() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function(){
+        console.log(this.responseText)
+    };
+    xhttp.open('GET', "api.php", true);
+    xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.send(request);
+}

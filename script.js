aa8547efb157b87c4b7a038430fcc2d55c6ecf76
Garza <mgarza@friendsbalt.org>
2022-05-20 15:23:30 -0400

    working on js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-
+let test= "test";
 function getRequest() {
     const xhttp = new XMLHttpRequest();
     xhttp.onload = function(){
-        console.log(this.responseText)
+        test=(this.responseText);
+        console.log(test);
     };
     xhttp.open('GET', "api.php", true);
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhttp.send();
 }
+
+const obj= JSON.parse(test);
+document.getElementById("quote").innerHTML=obj.quote;
